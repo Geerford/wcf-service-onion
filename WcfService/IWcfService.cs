bf94e35ed98326e7c2ebd31d6993d472b6920e0d
Сергей Милантьев <geerkus@gmail.com>
2019-02-06 18:11:40 +0400

    Add windows communication foundation service.

---
+++ WcfService/IWcfService.cs
@@ -0,0 +1,16 @@
+﻿using Service.DTO;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace WcfService
+{
+    [ServiceContract]
+    public interface IWcfService
+    {
+        [OperationContract]
+        IEnumerable<ClientDTO> GetClients();
+
+        [OperationContract]
+        IEnumerable<ClientDTO> GetOrders(int clientID);
+    }
+}
---
+++ WcfService/WcfService.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Domain.Core;
+using Infrastructure.Data;
+using Service.Business.Service;
+using Service.DTO;
+using Service.Interfaces;
+
+namespace WcfService
+{
+    public class WcfService : IWcfService
+    {
+        IService<Client> clientService;
+        IService<Item> itemService;
+        UnitOfWork unitOfWork;
+
+        public WcfService()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["Context"].ConnectionString;
+            unitOfWork = new UnitOfWork(connectionString);
+            clientService = new ClientService(unitOfWork);
+            itemService = new ItemService(unitOfWork);
+        }
+
+        public IEnumerable<ClientDTO> GetClients()
+        {
+            return clientService.GetAll().Select(x => new ClientDTO
+            {
+                ID = x.ID,
+                Name = x.Name,
+                Surname = x.Surname,
+                Midname = x.Midname
+            }).ToList();
+        }
+
+        public IEnumerable<ClientDTO> GetOrders(int clientID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
