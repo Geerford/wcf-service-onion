f5d1803d1e5ad4b79e8566c03bb808e695eca571
Сергей Милантьев <geerkus@gmail.com>
2019-02-06 18:10:28 +0400

    Add interfaces for services.

---
+++ Service.Interfaces/IService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Service.Interfaces
+{
+    public interface IService<T> where T : class
+    {
+        T Create(T model);
+
+        T Delete(T model);
+
+        void Dispose();
+
+        T Edit(T model);
+
+        T Get(int? id);
+
+        IEnumerable<T> GetAll();
+    }
+}
