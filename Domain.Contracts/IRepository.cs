28ad8af2fd3ac7de65ceb9b41596724c4d4f8ec6
Сергей Милантьев <geerkus@gmail.com>
2019-02-06 18:09:48 +0400

    Add UnitOfWork and Repository contracts.

---
+++ Domain.Contracts/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Contracts
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        T Get(int id);
+
+        IEnumerable<T> Find(Func<T, bool> predicate);
+
+        T FindFirst(Func<T, bool> predicate);
+
+        T Create(T item);
+
+        T Update(T item);
+
+        T Delete(int id);
+    }
+}
---
+++ Repository.Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Contracts;
+using Domain.Core;
+using System;
+
+namespace Repository.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Client> Client { get; }
+
+        IRepository<Item> Item { get; }
+
+        IRepository<Order> Order { get; }
+
+        void Save();
+    }
+}
