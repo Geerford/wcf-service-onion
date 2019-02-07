96c13606f058b03695800aad6cf0d3db99f6feb6
Сергей Милантьев <geerkus@gmail.com>
2019-02-08 00:05:59 +0400

    Add model for count requests.

---
+++ Service.DTO/ResponseHitsModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Service.DTO
+{
+    [DataContract]
+    public class ResponseHitsModel
+    {
+        [DataMember]
+        public int GetClientsRequest { get; set; }
+
+        [DataMember]
+        public int GetOrdersRequest { get; set; }
+
+        [DataMember]
+        public int GetGoodsRequest { get; set; }
+
+        [DataMember]
+        public int GetByClientRequest { get; set; }
+
+        [DataMember]
+        public int TotalRequest { get; set; }
+    }
+}
